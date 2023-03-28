943181130d55f2506934d69436e243ad4d527f60
LiXiang <993215231@qq.com>
2023-03-28 20:05:35 +0800

    hot-fix提交

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,7 +1,8 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("第二次提交写的东西");
+        System.out.println("第二次提交写的东西，hotfix也改了");
         System.out.println("第三次提交写的东西");
         System.out.println("第四次提交写的东西");
+        System.out.println("hot-fix test!");
     }
 }
